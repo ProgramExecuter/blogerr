ec5fac31320231864f1a6414e88b5630ab33f971
Amit Singh Rana <ranaa73000@gmail.com>
2022-07-18 22:44:22 +0530

    Adding basic user route logic

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,15 +1,17 @@
+import User from "../models/user.js";
+
 // Editing a User
 const editUser = async (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Edit user details of user id: ${req.params.userId}` });
+  await User.findByIdAndUpdate(req.params.userId, { $set: req.body });
+
+  res.status(200).json({ message: "User Edited" });
 };
 
 // Deleting a User
 const deleteUser = async (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Delete user with id: ${req.params.userId}` });
+  await User.findByIdAndDelete(req.params.userId);
+
+  res.status(200).json({ message: "Deleted User" });
 };
 
 export { editUser, deleteUser };
