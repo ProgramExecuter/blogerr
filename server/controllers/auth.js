c56daec647222bcd438fb83fb6798e6a196c04b4
Amit Singh Rana <ranaa73000@gmail.com>
2022-07-19 22:01:22 +0530

    Added basic Signup and Login logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,16 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 import User from "../models/user.js";
 
 // Signup a new User
 const signup = async (req, res) => {
-  const newUser = new User(req.body);
+  // Hash the password before saving in DB
+  const hash = bcrypt.hashSync(req.body.password, 8);
+  req.body.password = hash;
 
+  // Save the user in DB
+  const newUser = new User(req.body);
   await newUser.save();
 
   res.status(200).json(newUser);
@@ -11,7 +18,17 @@ const signup = async (req, res) => {
 
 // Login an existing User
 const login = async (req, res) => {
-  res.status(200).json({ message: "Login a user" });
+  // Find the user with login info
+  let userInfo = (await User.find({ username: req.body.username }))[0];
+
+  // Compare the hashed password with the password provided in login form
+  bcrypt.compare(req.body.password, userInfo.password).then((result) => {
+    if (result) {
+      res.status(200).json({ message: "Logged In" });
+    } else {
+      res.status(200).json({ message: "Wrong password" });
+    }
+  });
 };
 
 // Logout the current User
