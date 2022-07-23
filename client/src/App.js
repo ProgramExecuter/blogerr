1e2629e8bc4100358b17b57f0667717aefa198c1
Amit Singh Rana <ranaa73000@gmail.com>
2022-07-23 15:16:49 +0530

    Slight routing patch

--- client/src/App.js
+++ client/src/App.js
@@ -22,18 +22,21 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />}>
           <Route path="" element={<HomePage />} />
-          <Route path="/auth" element={<Auth />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+          <Route path="auth" element={<Auth />}>
+            <Route path="" element={<NotFound />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
           </Route>
-          <Route path="/post" element={<Post />}>
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/:postId" element={<ShowPost />} />
-            <Route path="/:postId/edit" element={<EditPost />} />
+          <Route path="post" element={<Post />}>
+            <Route path="" element={<NotFound />} />
+            <Route path="create" element={<CreatePost />} />
+            <Route path=":postId" element={<ShowPost />} />
+            <Route path=":postId/edit" element={<EditPost />} />
           </Route>
-          <Route path="/user" element={<User />}>
-            <Route path="/:userId" element={<UserDetails />} />
-            <Route path="/:userId/edit" element={<EditUser />} />
+          <Route path="user" element={<User />}>
+            <Route path="" element={<NotFound />} />
+            <Route path=":userId" element={<UserDetails />} />
+            <Route path=":userId/edit" element={<EditUser />} />
           </Route>
         </Route>
         <Route path="*" element={<NotFound />} />
